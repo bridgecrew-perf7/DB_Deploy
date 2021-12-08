6fa60bb3ef1faa2fabbacdec35b9cfff1c8af5eb
jyoo0515 <jasonyoo0515@gmail.com>
2021-12-09 05:09:03 +0900

    api fix

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const path = require("path");
 const socket = require("./socket");
 require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const app = express();
 const server = require("http").createServer(app);
